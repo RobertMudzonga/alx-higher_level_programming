f7d2b4e38d977c10905de09189da94a712301d39
RobertMudzonga <robertmudzonga@yahoo.com>
2021-11-18 06:11:31 -0800

    task 0 to 5

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv)
+    if num == 1:
+        print("{} arguments.".format(num - 1))
+    elif num == 2:
+        print("{} argument:".format(num - 1))
+    else:
+        print("{} arguments:".format(num - 1))
+
+    for r in range(1, num):
+        print("{}: {}".format(r, sys.argv[r]))
